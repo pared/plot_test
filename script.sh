5e228f7925459d6029fafc6ffb480377778d0a71
Paweł Redzyński <pawelredzynski@gmail.com>
2020-06-11 23:57:48 +0200

    adjust to new run arguments

--- script.sh
+++ script.sh
@@ -14,23 +14,23 @@ git commit -am "init"
 echo data >> data
 dvc add data -q
 
-dvc run -d data -o model -f train_model.dvc "cp data model"
+dvc run -d data -o model -n train_model "cp data model"
 
 cp $main/code.py code.py
-dvc run -q -d model -d code.py -M roc.csv -M loss_over_time.json -M confusion.csv -M feature_importance.csv -f evaluate.dvc "python code.py"
+dvc run -q -d model -d code.py --plots roc.csv --plots loss_over_time.json --plots confusion.csv -n evaluate "python code.py"
 
 git add -A
 git commit -am "first iteration"
 git tag v1
 
 cp $main/code1.py code.py
-dvc repro -q evaluate.dvc
+dvc repro -q evaluate
 
 git commit -am "second iteration"
 git tag v2
 
 cp $main/code2.py code.py
-dvc repro -q evaluate.dvc
+dvc repro -q evaluate
 
 git commit -am "third iteration"
 
